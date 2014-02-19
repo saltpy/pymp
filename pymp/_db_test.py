f87a1358cf84cb748d3c108d82ef2e5857d500bb
saltpy <saltpy@gmail.com>
2014-02-19 15:53:52 +0000

    Added: Query should remember the headers for its results

--- pymp/_db_test.py
+++ pymp/_db_test.py
@@ -29,17 +29,30 @@ class TestDb(unittest.TestCase):
                                      1)"""
         self.select_all = """SELECT * FROM people"""
 
+        self.manager = (1, u'Mick Manager', u'mick.manager@test.com', 4, 1)
+        self.tester = (2, u'Toby Tester', u'toby.tester@test.com', 3, 1)
+
     def tearDown(self):
         self.conn.close()
 
-    def test_execution_causes_db_to_change(self):
+    def _make_people_table(self):
         execute(self.conn, Query(self.create_sql))
         execute(self.conn, Query(self.insert_manager))
         execute(self.conn, Query(self.insert_tester))
-        expected = [(1, u'Mick Manager', u'mick.manager@test.com', 4, 1),
-                    (2, u'Toby Tester', u'toby.tester@test.com', 3, 1)]
+
+    def test_execution_causes_db_to_change(self):
+        self._make_people_table()
+        expected = [self.manager, self.tester]
 
         actual = execute(self.conn, Query(self.select_all)).results
+
+        self.assertEquals(expected, actual)
+
+    def test_a_query_should_record_the_headers_for_its_cursor(self):
+        self._make_people_table()
+        expected = ['id', 'name', 'email', 'level', 'manager']
+
+        actual = execute(self.conn, Query(self.select_all)).headers
 
         self.assertEquals(expected, actual)
 
