bdb579a5d90c7693e30ac074fa55d675165882f4
saltpy <saltpy@gmail.com>
2014-02-21 14:17:10 +0000

    Fixed: Build should use a virtualenv

--- build.sh
+++ build.sh
@@ -1,40 +1,36 @@
 #!/usr/bin/env bash
 
 ROOT='pymp'
+VIRTUALENV_WRAPPER='/usr/local/bin/virtualenvwrapper.sh'
 
 lint() {
-    flake8 $ROOT
-    if [ $? -ne 0 ]
-    then
-        exit $?
-    fi
+    flake8 $ROOT || exit $?
     echo "Lint passed!"
 }
 
 unittest() {
-    python -m unittest discover -s $ROOT -p '_*_test.py'
-    if [ $? -ne 0 ]
-    then
-        exit $?
-    fi
+    python -m unittest discover -s $ROOT -p '_*_test.py' || exit $?
     echo "Unittest passed!"
 }
 
 dev_install() {
-    pip install -r dev-requirements.pip
-    if [ $? -ne 0 ]
-    then
-        exit $?
-    fi
-    python setup.py develop
-    if [ $? -ne 0 ]
-    then
-        exit $?
-    fi
+    pip install -r dev-requirements.pip || exit $?
+    python setup.py develop || exit $?
     echo "Development install passed!"
 }
 
+resolve_virtualenv() {
+    if [ "$VIRTUAL_ENV" != "$WORKON_HOME/$ROOT" ]
+    then
+        . $VIRTUALENV_WRAPPER || exit $?
+        workon $ROOT || mkvirtualenv $ROOT || exit $?
+    fi
+    echo "Virtualenv resolution passed!"
+}
+
+resolve_virtualenv
 dev_install
 lint
 unittest
 echo "Build passed!"
+echo "Everything installed into $WORKON_HOME/$ROOT"
